defb36016ce59ba24393aefb5ba0d043f95fa097
laurenkro <lauren@colliver.io>
2025-04-15 10:07:19 -0400

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -19,7 +19,3 @@ git commit -m "added a new line to Shipping_Cost_Calculator.py"
 git status
 
 git push --set-upstream origin shipping_calculator_fixes
-
-# Here is another update by laurenkro
-git add .
-git commit -m "added additional line to Shipping_Cost_Calculator.py"
